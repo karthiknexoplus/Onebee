81052a94d37ebdfa3113f86fcfe4e64c0448bdf8
karthikdevarajiit <karthik.devarajiit@gmail.com>
2025-04-16 15:52:06 +0530

    Update host binding to 10.20.1.41

--- app.py
+++ app.py
@@ -1184,4 +1184,4 @@ def internal_error(error):
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=True) 
+    app.run(host='10.20.1.41', port=5000, debug=True) 
