c2fd56265180980748659447362b2372378ad0d6
karthikdevarajiit <karthik.devarajiit@gmail.com>
2025-04-16 15:25:35 +0530

    Update app.py to use 0.0.0.0 for host binding

--- app.py
+++ app.py
@@ -1149,4 +1149,4 @@ def internal_error(error):
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True, host="127.0.0.1", port=5000) 
+    app.run(host='10.20.1.41', port=5000, debug=True) 
